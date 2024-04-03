69c7b80005660d7768e93dab1b77cd40a1a8c5b0
textile <thex1le@gmail.com>
2024-04-03 11:33:26 -0400

    pep8

--- GLaDOSSenses.py
+++ GLaDOSSenses.py
@@ -12,7 +12,8 @@ from pickle import dumps, loads
 import zmq
 import cv2
 
-class GLaDOS_Server_Exception(Exception):
+
+class GLaDOSServerException(Exception):
     pass
 
 
@@ -30,7 +31,8 @@ class Camera(Thread):
         if self.picam is True:
             # pi cam
             self.cap = Picamera2()
-            self.cap.configure(self.cap.create_preview_configuration({"size": (self.cam_res_x, self.cam_res_y), 'format': 'RGB888'}))
+            self.cap.configure(self.cap.create_preview_configuration({"size": (self.cam_res_x, self.cam_res_y),
+                                                                      'format': 'RGB888'}))
             self.cap.start()
         else:
             # usb webcam
@@ -73,7 +75,7 @@ class Camera(Thread):
 
 class YoloDetect(Thread):
     def __init__(self, configfile):
-        #internal libs, import here so its deps are not needed on other devices
+        # internal libs, import here so its deps are not needed on other devices
         import GLaDOSVision
         Thread.__init__(self)
         Thread.daemon = True
@@ -133,7 +135,7 @@ class YoloDetect(Thread):
         return results
 
     def process_image(self, image):
-        final_image=loads(image) 
+        final_image = loads(image)
         cv2.imwrite('raw_rx.jpg', final_image)
         self.sight = self.__yolo_process_image(final_image)
         print("sending back dict")
@@ -209,7 +211,7 @@ class DataRecv(Thread):
 
     def run(self):
         while self.stop is False:
-            #TODO check if there is a timeout here or it will never close on exit?
+            # TODO check if there is a timeout here or it will never close on exit?
             self.data = self.socket.recv()  # Receive the data as a JSON string
             print("got data")
         self.socketsend.close()
@@ -221,18 +223,19 @@ if __name__ == "__main__":
     parser.add_argument('-config', type=str, default=1, dest='conf', nargs=1, help='Config File')
     try: 
         args = parser.parse_args()
-    except:
+    except Exception:
         parser.print_help()
         sys.exit(0)
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     configp = configparser.ConfigParser()
     if path.isfile(args.conf[0]) is True:
         configp.read(args.conf[0])
     else:
-        raise GLaDOS_Server_Exception("Unable to load file {}".format(args.conf[0]))
-    # import and init the 3rd part glados text to speach engine, this prevents init of the engine when you just want to print the help
+        raise GLaDOSServerException("Unable to load file {}".format(args.conf[0]))
+    # import and init the 3rd part glados text to speach engine,
+    # this prevents init of the engine when you just want to print the help
     from gladosTTS import engine
     from ultralytics import YOLO
     from ultralytics.utils.plotting import Annotator
@@ -240,11 +243,9 @@ if __name__ == "__main__":
     eyes.start()
     # start the text to speech engine
     engine.main()
-    #ttsengine = mp.Process(target=engine.main, args=())
-    #ttsengine.start()
+    # ttsengine = mp.Process(target=engine.main, args=())
+    #t tsengine.start()
     # loop
     # do we keep looping hear? what blocks on main?
-    #while True:
+    # while True:
     #    time.sleep(1)
-        
-
