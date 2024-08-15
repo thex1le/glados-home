0a39f1a1e5a5c66b46bc6bff5496f19e9460ad71
textile <thex1le@gmail.com>
2024-08-15 13:06:30 -0400

    add checks to not create double loggers

--- glados_modules/GlogConfig.py
+++ glados_modules/GlogConfig.py
@@ -7,16 +7,20 @@ def setup_logger(name=__name__, level=logging.DEBUG, log_dir="logs",
                  file_logging=logging.DEBUG, console_logging=logging.DEBUG):
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    if not path.exists(log_dir):
-        makedirs(log_dir)
-    fh = logging.FileHandler(path.join(log_dir, name))
-    fh.setLevel(file_logging)
-    ch = logging.StreamHandler()
-    ch.setLevel(console_logging)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-    logger.getEffectiveLevel()
+    # Check if the logger already has handlers to avoid adding them multiple times
+    if not logger.hasHandlers():
+        if not path.exists(log_dir):
+            makedirs(log_dir)
+        # Ensure log file has a .log extension
+        log_file = path.join(log_dir, f"{name}.log")
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(file_logging)
+        ch = logging.StreamHandler()
+        ch.setLevel(console_logging)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
     return logger
