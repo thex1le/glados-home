9c8620a024177d67d506cd85c26e4e12537ee8af
textile <thex1le@gmail.com>
2024-08-17 09:24:35 -0400

    fix up camera module for direct testing

--- glados_modules/Camera.py
+++ glados_modules/Camera.py
@@ -96,5 +96,30 @@ class Camera(Process, MQTTClient):
                 print(time.time() - t)
 
 
+if __name__ == "__main__":
+    # imports only for testing
+    from argparse import ArgumentParser
+    import sys
+    from configparser import ConfigParser
+    from os import path
+    parser = ArgumentParser(description='Evil Home AI Senses Server')
+    parser.add_argument('-config', type=str, default=1, dest='conf', nargs=1, help='Config File')
+    try:
+        args = parser.parse_args()
+    except Exception:
+        parser.print_help()
+        sys.exit(0)
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    config_p = ConfigParser()
+    if path.isfile(args.conf[0]) is True:
+        config_p.read(args.conf[0])
+    else:
+        raise GLaDOSServerException("Unable to load file {}".format(args.conf[0]))
+    head_camera_location = config_p["CAMERAS"]["Camera_Head_Factory"]
+    head_camera = Camera(configfile=config_p, location=head_camera_location)
+
+
 
 
