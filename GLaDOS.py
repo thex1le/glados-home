8ba81052e8c500bbf9accdb55dbf4e6b104f2fa4
textile <thex1le@gmail.com>
2024-09-22 22:27:10 -0400

    dont flip rotation direction for side cameras

--- GLaDOS.py
+++ GLaDOS.py
@@ -146,7 +146,7 @@ class MotionTrack(MQTTClient):
                     elif camera in (TrackingEnums.BODY_LEFT_CAMERA.value, TrackingEnums.BODY_RIGHT_CAMERA.value):
                         if self.side_camera_count <= 5:
                             self.logger.debug(f"Rotating Body to face target {self.target}")
-                            self.rotate_body(target=target_bounding, flip=True)
+                            self.rotate_body(target=target_bounding)
                             self.side_camera_count += 1
                             # hold for a while to let main camera capture targets
                             time.sleep(5)
