495e924a08244f9280403dd80c4a8d7dd467cd99
thex1le <thex1le@gmail.com>
2024-12-29 21:59:33 -0500

    fix annoying / bug

--- glados_modules/RtspServer.py
+++ glados_modules/RtspServer.py
@@ -68,12 +68,12 @@ class RTSPServer(GstRtspServer.RTSPServer):
     def _initialize_factories(self, cam_configs):
         mount_points = self.get_mount_points()
         for factory_path in cam_configs.keys():
-            if factory_path[0] != '/':
-                # append a / if needed
-                factory_path = "/" + factory_path
             (cam_x, cam_y) = cam_configs[factory_path][CameraEnum.MSG_RESOLUTION.value]
             rtsp_system = RtspSystem(cam_x, cam_y, int(cam_configs[factory_path][CameraEnum.MSG_FPS.value]))
             rtsp_system.set_shared(True)
+            if factory_path[0] != '/':
+                # append a / if needed
+                factory_path = "/" + factory_path
             mount_points.add_factory(factory_path, rtsp_system)
             rtsp_system.start()
             self.factories[factory_path] = rtsp_system
