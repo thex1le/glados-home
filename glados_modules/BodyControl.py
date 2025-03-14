ac8179a6d07e27b00654e218c835b07a99779b16
thex1le <thex1le@gmail.com>
2025-03-14 00:36:41 -0400

    fix up led body control command

--- glados_modules/BodyControl.py
+++ glados_modules/BodyControl.py
@@ -987,13 +987,13 @@ class LedHead(MQTTClient):
 
     def handle_cmd(self, msg: MQTTMessage) -> None:
         j_msg = loads(msg.payload.decode())
-        msg = j_msg.get(LEDHead.MSG_COMMAND_KEY.value, {})
-        if msg.get(LEDHead.MSG_COMMAND_LOCATION_KEY.value, "") == self.location:
+        body = j_msg.get(LEDHead.MSG_COMMAND_KEY.value, {})
+        if body.get(LEDHead.MSG_COMMAND_LOCATION_KEY.value, "") == self.location:
             self.logger.debug(f"{self.location}, {msg.topic},  {j_msg}")
-            animation_key = msg.get(LEDHead.MSG_COMMAND_KEY.value, "")
+            animation_key = body.get(LEDHead.MSG_COMMAND_KEY.value, "")
             if animation_key in self.animations.keys():
                 if animation_key == LEDHead.ANIMATION_SPEECH_EYE_KEY.value:
-                    args = msg.get(LEDHead.MSG_COMMAND_ARGUMENTS_KEY.value, '')
+                    args = body.get(LEDHead.MSG_COMMAND_ARGUMENTS_KEY.value, '')
                     if args != '':
                         self.animations[animation_key](args)
                 else:
